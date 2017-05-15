3410277ff12201f8ed223f54c9e9d445af1f9da5
Neil Satterfield <neilsatt@gmail.com>
2017-05-14 22:00:52 -0600

    initial commit

---
+++ js/video.js
@@ -0,0 +1,8 @@
+// Video Player Options
+$(document).ready(function(){
+    $('video').mediaelementplayer({
+            features: ['playpause', 'tracks', 'progress', 'tracksText', 'fullscreen', 'volume'],
+            startLanguage: 'en'
+    })
+});
+          
